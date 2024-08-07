c3d10fb4605b21dc75a2a43bd3647a15003cc15a
HermanPlay <hermanplay1110@gmail.com>
2024-08-07 19:59:35 +0200

    workers working :D

--- worker_init.sh
+++ worker_init.sh
@@ -4,13 +4,13 @@ set -x
 
 sudo apt install -y proot
 
-/sio2/oioioi/wait-for-it.sh -t 60 "oioioi-dev_db:5432"
-/sio2/oioioi/wait-for-it.sh -t 0  "oioioi-dev_web:8000"
+/sio2/oioioi/wait-for-it.sh -t 60 "oioioi_db:5432"
+/sio2/oioioi/wait-for-it.sh -t 0  "oioioi_web:8000"
 
 mkdir -pv /sio2/deployment/logs/database
 
 echo "LOG: Launching worker at `hostname`"
-export FILETRACKER_URL="http://oioioi-dev_web:9999"
+export FILETRACKER_URL="http://oioioi_web:9999"
 exec python3 $(which twistd) --nodaemon --pidfile=/home/oioioi/worker.pid \
         -l /sio2/deployment/logs/worker`hostname`.log worker \
         --can-run-cpu-exec \
